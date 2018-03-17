a4a99c3a16b46769459f4fd38a8d6d65662e8d85
Dylan <Dylan@Dylans-MBP.hsd1.ca.comcast.net>
2018-03-16 23:05:53 -0700

    Added Form Validation, Confirmation Dialog, and Social Media Section

--- js/app.js
+++ js/app.js
@@ -4,34 +4,73 @@ courtDev.config(function (RestangularProvider) {
     RestangularProvider.setBaseUrl('http://courtdev-api.azurewebsites.net/');
 });
 
-courtDev.controller("splashController", function($scope, $log, Restangular){
+courtDev.controller("splashController", function($scope, $log, Restangular, $http){
     
     var vm = this;
     vm.promiseAwait = false;
     vm.registerForm = {};
+    vm.courts = [];
 
-    vm.register = function(){
+    init();    
 
-        vm.promiseAwait = true;
-        var registerObj = {
-            "name": vm.registerForm.name,
-            "court": vm.registerForm.court,
-            "email": vm.registerForm.email,
-            "jobRole": vm.registerForm.role,
-            "phone": vm.registerForm.phoneNumber
-        };
-        console.log(registerObj);
+    function init () {
+        getCourts();
+    }
 
-        Restangular.all('api/registration').customPOST(registerObj).then(function (resp) {
-            console.log("return");
-            console.log(resp);
-            vm.promiseAwait = false;
-        }, function (err) {
-            vm.promiseAwait = false;
-            $log.error(err);
-        });
+    function getCourts () {
+        $http.get('/data.json').then(function(resp) {
+            vm.courts = resp.data['Counties'];
+         });
+    }
 
-  
+    vm.register = function(registerForm){
+        if (registerForm.$invalid) {
+            angular.element('#registerForm').addClass("was-validated")
+        }
+        else {
+            vm.promiseAwait = true;
+            var registerObj = {
+                "name": vm.registerForm.name,
+                "court": vm.registerForm.court,
+                "email": vm.registerForm.email,
+                "jobRole": vm.registerForm.role,
+                "phone": vm.registerForm.phoneNumber
+            };
+            console.log(registerObj);
+    
+            Restangular.all('api/registration').customPOST(registerObj).then(function (resp) {
+                console.log("return");
+                console.log(resp);
+                swal({
+                    title: "Thank you for registering!",
+                    text: "We're looking forward to seeing you at the event!",
+                    icon: "success",
+                    buttons: false,
+                    timer: 5000
+                });              
+                vm.promiseAwait = false;
+                resetForm();
+            }, function (err) {
+                swal({
+                    title: "We encountered an error in your submission.",
+                    text: "Please try again soon!",
+                    icon: "error",
+                    buttons: false,
+                    timer: 5000
+                });       
+                vm.promiseAwait = false;
+                $log.error(err);
+            });
+        }
+
+        function resetForm() {
+            angular.element('#registerForm').removeClass("was-validated")
+            vm.registerForm.name = "";
+            vm.registerForm.court = "";
+            vm.registerForm.email = "";
+            vm.registerForm.role = "";
+            vm.registerForm.phoneNumber = "";
+        }
     }
 
    
