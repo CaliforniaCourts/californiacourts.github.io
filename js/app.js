a4f93fb38a76b1ef28cc6d749ee58a6d39d3e2f0
Dylan Demmers <ddemmers@scscourt.org>
2019-01-16 16:25:42 -0800

    Add counter to home page.

--- js/app.js
+++ js/app.js
@@ -1,4 +1,4 @@
-var courtDev = angular.module("courtDev", ['restangular']);
+var courtDev = angular.module("courtDev", ['restangular', 'countUpModule']);
 
 courtDev.config(function (RestangularProvider) {
     RestangularProvider.setBaseUrl('https://courtdev-api.azurewebsites.net/');
@@ -10,10 +10,15 @@ courtDev.controller("splashController", function($scope, $log, Restangular, $htt
     vm.promiseAwait = false;
     vm.registerForm = {};
     vm.courts = [];
+    vm.firstVisit = true;
+    vm.timer;
+    vm.eventDate = new Date("2019-02-22T09:00:00-08:00")
 
     init();    
 
     function init () {
+        getAttendeeCount();
+        startTimer();
         getCourts();
     }
 
@@ -21,6 +26,59 @@ courtDev.controller("splashController", function($scope, $log, Restangular, $htt
         $http.get('/data.json').then(function(resp) {
             vm.courts = resp.data['Counties'];
          });
+    }
+
+    function startTimer() {
+        vm.timer = setInterval(function() {
+            timeBetweenDates(vm.eventDate);
+        }, 1000);
+    }
+
+
+
+    function timeBetweenDates(toDate) {
+    var dateEntered = toDate;
+    var now = new Date();
+    var difference = dateEntered.getTime() - now.getTime();
+
+    if (difference <= 0) {
+        // Event Reached
+        clearInterval(vm.timer);
+    } else {
+        var seconds = Math.floor(difference / 1000);
+        var minutes = Math.floor(seconds / 60);
+        var hours = Math.floor(minutes / 60);
+        var days = Math.floor(hours / 24);
+
+        hours %= 24;
+        minutes %= 60;
+        seconds %= 60;
+
+        if (vm.firstVisit === true) {
+            var secAnim = new CountUp("seconds", 0, seconds);
+            secAnim.start();
+            var minAnim = new CountUp("minutes", 0, minutes);
+            minAnim.start();
+            var hrAnim = new CountUp("hours", 0, hours);
+            hrAnim.start();
+            var dayAnim = new CountUp("days", 0, days);
+            dayAnim.start();
+            vm.firstVisit = false;
+        } 
+
+        angular.element("#days").text(days);
+        angular.element("#hours").text(hours);
+        angular.element("#minutes").text(minutes);
+        angular.element("#seconds").text(seconds);
+    }
+    }
+
+    function getAttendeeCount () {
+        Restangular.one("api/registration/count").get().then(function (resp) {
+            vm.attendeeCount = resp;
+            var numAnim = new CountUp("count", 0, vm.attendeeCount);
+            numAnim.start();
+        })
     }
 
     vm.register = function(registerForm){
